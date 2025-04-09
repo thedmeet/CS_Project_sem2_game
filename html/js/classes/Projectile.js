46dfcba86220637394fcab205a908d76b364094d
thedmeet <meetdhorajiya415@gmail.com>
2025-04-09 13:30:32 +0530

    Projectile.js

--- html/js/classes/Projectile.js
+++ html/js/classes/Projectile.js
@@ -17,7 +17,7 @@ class Projectile extends Sprite {
       this.enemy.center.x - this.position.x
     )
 
-    const power = 5
+    const power = 8
     this.velocity.x = Math.cos(angle) * power
     this.velocity.y = Math.sin(angle) * power
 
